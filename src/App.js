feb278181d7329fe4731b0ee7752f22b418b4d8a
Mohamed salim Gourari <mohasalim.gourari@gmail.com>
2019-04-01 17:20:13 +0200

    md-editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import marked from 'marked'
+
+import { sampleText } from './sampleText'
+
 class App extends Component {
+
+  state = {
+    text: sampleText
+  }
+
+  componentDidUpdate () {
+    const { text } = this.state
+    localStorage.setItem('text', text) 
+  }
+
+  componentDidMount () {
+
+    const text = localStorage.getItem('text')
+
+    if (text) {
+      this.setState({text})
+    }
+    else {
+      this.setState({ text: sampleText })
+    }
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true})
+    return { __html }
+  
+  }
+  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div id="app-wall" style={{ backgroundColor: "rgba(0, 204, 204, .15)" }}>
+       <div id="app">
+       <header>fichier.md
+            <div class="buttons"></div>
         </header>
+        <div class="editor">
+        <textarea class="code" style={{ color: "white" }} onChange={this.handleChange} value={ this.state.text }></textarea>
+        <div dangerouslySetInnerHTML={this.renderText(this.state.text)} class="result"></div>
+        </div>
+       </div>
+      
+      
       </div>
     );
   }
